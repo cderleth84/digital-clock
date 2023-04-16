a45b848f95d4bb2c81e48584dbfcc8dfbc3b83f4
Christopher Derleth <christopher@ChristophersAir.attlocal.net>
2023-04-16 09:17:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const hoursElement = document.getElementById("hour");
+const minutesElement = document.getElementById("minutes");
+const secondsElement = document.getElementById("seconds");
+const ampmElement = document.getElementById("ampm");
